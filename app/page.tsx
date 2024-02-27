233305232c81a06ba7094f89435eb3d809501c86
Uy <uynguyen1501@gmail.com>
2024-02-27 21:49:55 +0100

    Section divier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/Intro";
+import SectionDivder from "@/components/SectionDivder";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro/>
+      <SectionDivder/>
     </main>
   )
 }
