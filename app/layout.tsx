0ce0bb370d502e9e765f4899f2c096714ef3a75a
Uy <uynguyen1501@gmail.com>
2024-02-27 16:32:49 +0100

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +24,7 @@ export default function RootLayout({
         <div
             className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
-
+        <Header/>
         {children}
         </body>
         </html>
